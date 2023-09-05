1954f8568bbd5bdc558d5d1ad82e396fdb96ef57
akshithcsueb <akatragada@horizon.csueastbay.edu>
2023-09-05 09:58:24 -0500

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+interface FormResult {
+  name?: string;
+  dob?: string;
+  gender?: string;
+  languages?: string[];
+  country?: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'forms-using-angular';
+  name: string = '';
+  dob: string = '';
+  gender: string = '';
+  languages: string[] = [];
+  country: string = '';
+  result: FormResult = {};
+
+  updateLanguages(language: string, event: any) {
+    if (event.target.checked && !this.languages.includes(language)) {
+      this.languages.push(language);
+    } else if (!event.target.checked && this.languages.includes(language)) {
+      this.languages = this.languages.filter((lang) => lang !== language);
+    }
+  }
+
+  submitForm() {
+    this.result = {
+      name: this.name,
+      dob: this.dob,
+      gender: this.gender,
+      languages: this.languages,
+      country: this.country,
+    };
+  }
 }
